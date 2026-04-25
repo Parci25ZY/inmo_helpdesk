76b6191e95c7b0fe9ed097b8e019aed2c5636167
Parci25ZY <parcivaljy5.gmail.com>
2026-04-25 17:25:01 -0500

    Paneles de usuarios

--- apps/tickets/views.py
+++ apps/tickets/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Ticket
 
-# Create your views here.
+class DashboardView(LoginRequiredMixin, ListView):
+    """
+    Vista principal del sistema. 
+    Filtra los tickets según el rol del usuario autenticado.
+    """
+    model = Ticket
+    template_name = 'tickets/dashboard.html'
+    context_object_name = 'tickets'
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.rol == user.Roles.TECNICO:
+            return Ticket.objects.all().select_related('inquilino', 'tecnico')
+        else:
+            return Ticket.objects.filter(inquilino=user).select_related('tecnico')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        queryset = self.get_queryset()
+        context['pendientes'] = queryset.filter(estado='PENDIENTE').count()
+        context['en_progreso'] = queryset.filter(estado='EN_PROGRESO').count()
+        return context
--- config/urls.py
+++ config/urls.py
@@ -2,12 +2,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from apps.tickets.views import DashboardView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Autenticación (Login/Logout)
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', DashboardView.as_view(), name='dashboard'),
     path("__reload__/", include("django_browser_reload.urls")),
     
 ]
